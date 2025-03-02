2c8453a01b8a3b35b9e6a76412d127d3105c21b0
swarnaranjani uyyuru <swarnaranjani@gmail.com>
2025-03-02 19:37:57 +0530

    fix5

--- app/financial_rag.py
+++ app/financial_rag.py
@@ -2,9 +2,9 @@ import os
 import numpy as np
 from typing import List, Tuple, Dict, Any
 import faiss
-from preprocessing import preprocess_documents, chunk_text
-from hybrid_search import hybrid_search
-from llm_generator import generate_response
+from app.preprocessing import preprocess_documents, chunk_text
+from app.hybrid_search import hybrid_search
+from app.llm_generator import generate_response
 import pickle
 import logging
 from config import VECTOR_DB_PATH, CHUNKS_PATH, CHUNKS_METADATA_PATH, EMBEDDINGS_MODEL_NAME
